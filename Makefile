357af0ec68071af8def00632f967a4bc6259315e
anvztor <15998375+anvztor@users.noreply.github.com>
2024-11-09 15:29:58 +0800

    chore: reinit genesis

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
-init: clean goat geth contracts
+init: precheck clean goat geth contracts
+	cp example.json config.json
+	sh ./init.sh
+	command -v pm2 || npm install pm2 -g
+
+start:
+	pm2 start ./build/geth -- --datadir ./data/geth --gcmode=archive --goat.preset=rpc --nodiscover
+	pm2 start ./build/goatd -- start --home ./data/goat --regtest --goat.geth ./data/geth/geth.ipc
+
+stop:
+	pm2 delete all || echo "stopped"
+	pm2 flush
+
+logs:
+	pm2 logs all
 
 goat:
 	mkdir -p build data/goat
@@ -11,12 +25,34 @@ geth:
 	cp submodule/geth/build/bin/geth build
 
 contracts:
-	npm ci --prefix submodule/contracts
-	npm --prefix submodule/contracts run compile
+	npm ci --engine-strict --prefix submodule/contracts
+	npm --prefix submodule/contracts --engine-strict run compile
 
-clean:
+clean: stop
 	rm -rf build
 	rm -rf data/goat data/geth
+	rm -rf config.json
+	rm -rf submodule/contracts/artifacts
+	rm -rf submodule/contracts/cache
+	rm -rf submodule/contracts/genesis/regtest-config.json
+	rm -rf submodule/contracts/genesis/regtest.json
+	rm -rf submodule/contracts/typechain-types
+	rm -rf submodule/contracts/node_modules
+	rm -rf submodule/goat/build
+	rm -rf submodule/geth/build/bin
+
+web3:
+	@./build/geth attach --datadir ./data/geth
+
+precheck:
+	node --version
+	go version
+	docker --version
+	docker compose version
+	jq --version
+
+update:
+	git submodule update
 
 docker-goat:
 	cp Makefile.goat submodule/goat/Makefile.goat
--- init.sh
+++ init.sh
@@ -9,12 +9,12 @@ OWNER='0xbc000FE892bC88F2ba41d70aF9F80619F556dCA2'
 VALIDATOR=$(./build/goatd --home ./data/goat modgen locking sign --owner $OWNER)
 jq --argjson new_data "$VALIDATOR" '.Locking.validators += [$new_data]' config.json > tmp.json && mv tmp.json config.json
 
-VOTER=$(./build/goatd --home ./data/goat modgen relayer keygen --output 1.json)
-jq --argjson new_data "$VOTER" '.Relayer.voters += [$new_data]' config.json > tmp.json && mv tmp.json config.json
-VOTER=$(./build/goatd --home ./data/goat modgen relayer keygen --output 2.json)
-jq --argjson new_data "$VOTER" '.Relayer.voters += [$new_data]' config.json > tmp.json && mv tmp.json config.json
-VOTER=$(./build/goatd --home ./data/goat modgen relayer keygen --output 3.json)
-jq --argjson new_data "$VOTER" '.Relayer.voters += [$new_data]' config.json > tmp.json && mv tmp.json config.json
+VOTERA=$(./build/goatd --home ./data/goat modgen relayer keygen --output 1.json)
+jq --argjson new_data "$VOTERA" '.Relayer.voters += [$new_data]' config.json > tmp.json && mv tmp.json config.json
+VOTERB=$(./build/goatd --home ./data/goat modgen relayer keygen --output 2.json)
+jq --argjson new_data "$VOTERB" '.Relayer.voters += [$new_data]' config.json > tmp.json && mv tmp.json config.json
+VOTERC=$(./build/goatd --home ./data/goat modgen relayer keygen --output 3.json)
+jq --argjson new_data "$VOTERC" '.Relayer.voters += [$new_data]' config.json > tmp.json && mv tmp.json config.json
 
 npm --prefix submodule/contracts run genesis -- --param ../../config.json --faucet $OWNER --amount 1000
 ./build/geth init --state.scheme hash --cache.preimages --datadir ./data/geth ./submodule/contracts/genesis/regtest.json
